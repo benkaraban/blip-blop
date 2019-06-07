97e64401aeef8cbff067491cc250dee665d07160
Guillaume 'Vermeille' Sanchez <guillaume.v.sanchez@gmail.com>
2019-06-07 18:54:10 +0200

    format

--- vc-projects/Blip_n_Blop_3/dd_gfx.h
+++ vc-projects/Blip_n_Blop_3/dd_gfx.h
@@ -1,32 +1,30 @@
 /******************************************************************
-*
-*
-*		----------------
-*		  DD_gfx.h
-*		----------------
-*
-*
-*		Fonctions graphiques PROPRES
-*
-*
-*		Prosper / LOADED -   V 0.1
-*
-*
-*
-******************************************************************/
-
+ *
+ *
+ *		----------------
+ *		  DD_gfx.h
+ *		----------------
+ *
+ *
+ *		Fonctions graphiques PROPRES
+ *
+ *
+ *		Prosper / LOADED -   V 0.1
+ *
+ *
+ *
+ ******************************************************************/
 
 #ifndef _GFX_UTIL_
 #define _GFX_UTIL_
-
 
 //-----------------------------------------------------------------------------
 //		Constantes pour la création des surfaces
 //-----------------------------------------------------------------------------
 
-#define DDSURF_VIDEO	1	// Mettre la surface en mémoire VIDEO
-#define DDSURF_SYSTEM	2	// Mettre la surface en mémoire SYSTEM
-#define DDSURF_BEST		4	// Mettre la surf en VIDEO si possible, SYSTEM sinon
+#define DDSURF_VIDEO 1   // Mettre la surface en mémoire VIDEO
+#define DDSURF_SYSTEM 2  // Mettre la surface en mémoire SYSTEM
+#define DDSURF_BEST 4    // Mettre la surf en VIDEO si possible, SYSTEM sinon
 
 //-----------------------------------------------------------------------------
 //		Headers
@@ -46,20 +44,20 @@ extern Graphics* graphicInstance;
 //		Prototypes
 //-----------------------------------------------------------------------------
 
-bool					DDInitDirectDraw();
-bool					DDSetGfxMode(int x, int y, int d);
-void					DDCloseDirectDraw();
-SDL::Surface *	DDCreatePrimary();
-SDL::Surface *	DDCreatePrimary(SDL::Surface * & back);
-SDL::Surface *	DDCreateSurface(int x, int y);
-SDL::Surface *	DDCreateSurface(int x, int y, int flags);
-SDL::Surface *	DDLoadBMP(char * file);
-SDL::Surface *	DDLoadBMP(char * file, int flags);
-HRESULT					DDCopyBMP(SDL::Surface *surf, HBITMAP hbm);
-void *	DDLoadPalette(char * file);
-DWORD					DDFindColor(SDL::Surface *surf, COLORREF rgb);
-HRESULT					DDSetColorKey(SDL::Surface *surf, COLORREF rgb);
-void					DDFlip();
-void					DDFlipV();
+bool DDInitDirectDraw();
+bool DDSetGfxMode(int x, int y, int d);
+void DDCloseDirectDraw();
+SDL::Surface* DDCreatePrimary();
+SDL::Surface* DDCreatePrimary(SDL::Surface*& back);
+SDL::Surface* DDCreateSurface(int x, int y);
+SDL::Surface* DDCreateSurface(int x, int y, int flags);
+SDL::Surface* DDLoadBMP(char* file);
+SDL::Surface* DDLoadBMP(char* file, int flags);
+HRESULT DDCopyBMP(SDL::Surface* surf, HBITMAP hbm);
+void* DDLoadPalette(char* file);
+DWORD DDFindColor(SDL::Surface* surf, COLORREF rgb);
+HRESULT DDSetColorKey(SDL::Surface* surf, COLORREF rgb);
+void DDFlip();
+void DDFlipV();
 
 #endif
